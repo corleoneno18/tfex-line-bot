8136008e94e0ec4011e0013a1f7cd76d376dea53
corleoneno18 <corleone.no.18@gmail.com>
2026-09-25 15:35:45 +0700

    Update main.py

--- main.py
+++ main.py
@@ -26,7 +26,7 @@ def summarize_with_gemini(raw_data):
     """
     
     response = client.models.generate_content(
-        model="gemini-2.5-flash",
+        model="gemini-1.5-flash",
         contents=prompt
     )
     return response.text
